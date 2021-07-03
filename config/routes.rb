28449aff5ca4adfb4644a2da074c2fe88ed0d0ef
Saulo Lira <saulo.l.nascimento@hotmail.com>
2021-07-03 16:30:05 -0300

    Adicione versionamento da api com a gem versionist

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :kinds
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # Separando as versões dos controllers em v1 e v2
+=begin Separando as versões dos controllers em v1 e v2 (Sem gem, com rails nativo)
 
   # Para acessar essa rota: http://localhost:3000/contacts?version=1
   scope module: 'v1' do
@@ -15,4 +15,16 @@ Rails.application.routes.draw do
     resources :contacts, :constraints => lambda { |request| request.params[:version] == '2'} do
     end
   end
+=end
+
+  # Separando as versões dos controllers em v1 e v2 (Com a gem versionist: https://github.com/bploetz/versionist)
+
+  api_version(:module => "v1", :parameter => {:name => "version", :value => "1"}) do
+    resources :contacts
+  end
+
+  api_version(:module => "v2", :parameter => {:name => "version", :value => "2"}) do
+    resources :contacts
+  end
+  
 end
